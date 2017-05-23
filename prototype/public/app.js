8b54085ff79d2e5293b7d51b611e2d910b2c3765
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-05-23 15:30:11 -0700

    molFile being reRendered properly now with correct function

--- prototype/public/app.js
+++ prototype/public/app.js
@@ -31,13 +31,14 @@ var molFile;
 
 $('#button').on('click',function(){
      //grabs drawn molecule and generates molfile while also replacing new line characters with \n
-     var content = chemwriter.components['editor'].getMolfile().replace(/\r?\n/g, '\\n');
+     var content = chemwriter.components['editor'].getMolfile();
      $("#data-container-4").html(content);
 
+     //content.replace(/\r?\n/gm, '\\n');
      //update molFile global variable
      molFile = content;
 
-     console.log(typeof molFile);
+     console.log(molFile);
 
 });
 
@@ -51,60 +52,7 @@ $('#clearChem').on('click',function(){
 //ChemWriter reRender molFile
 $('#reRenderChem').on('click',function(){
      console.log('reRendering editor');
-     $('#editor').attr('data-chemwriter-data', "[NO NAME]\n  CHEMW2  02061315242D                              \nCreated with ChemWriter - http://chemwriter.com\n 12 12  0  0  1  0            999 V2000\n    5.6944   -5.5278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    6.5605   -6.0278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    7.4265   -5.5278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    7.4265   -4.5278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    6.5605   -4.0278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    5.6944   -4.5278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    8.2925   -4.0278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    9.1585   -4.5278    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   10.0246   -4.0278    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0\n    8.2925   -3.0278    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n    4.8284   -4.0278    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n    4.8284   -6.0278    0.0000 O   0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  2  0  0  0  0\n  2  3  1  0  0  0  0\n  3  4  2  0  0  0  0\n  4  5  1  0  0  0  0\n  5  6  2  0  0  0  0\n  6  1  1  0  0  0  0\n  4  7  1  0  0  0  0\n  7  8  1  0  0  0  0\n  8  9  1  0  0  0  0\n  7 10  1  4  0  0  0\n  6 11  1  0  0  0  0\n  1 12  1  0  0  0  0\nM  END\n");
 
-});
-
-
-/*
-//loads chemDoodle plugin
-var sketcher = new ChemDoodle.SketcherCanvas('sketcher', 500, 300, {useServices:true, oneMolecule:true});
-
-
-//get chemDoodle content
-
-var molecule;
-
-$('#sketch_submit').on('click',function(){
-     console.log('ChemDoodle captured');
-     //counts the number of atoms
-     var mol = sketcher.getMolecule();
-     console.log(mol);
-
-     //generates MOlfile
-     var molFile = ChemDoodle.writeMOL(mol);
-     console.log(molFile);
-
-     //updates molecule global variable
-     molecule = molFile;
-
-     console.log ("molecule global variable has been updated to: " + molecule);
-
-     //view canvas as an image
-     var viewACS = new ChemDoodle.ViewerCanvas('viewACS', 100, 100);
-       viewACS.specs.bonds_width_2D = .6;
-       viewACS.specs.bonds_saturationWidth_2D = .18;
-       viewACS.specs.bonds_hashSpacing_2D = 2.5;
-       viewACS.specs.atoms_font_size_2D = 10;
-       viewACS.specs.atoms_font_families_2D = ['Helvetica', 'Arial', 'sans-serif'];
-       viewACS.specs.atoms_displayTerminalCarbonLabels_2D = true;
-       var molecule = ChemDoodle.readMOL(molFile);
-      viewACS.loadMolecule(molecule);
-
-
-     $("#data-container-3").html(molFile);
-     //$("#data-container-3").html(molecule);
-
-});
-
-//reRender ChemDoodle content
-$('#reRender').on('click', function(){
-     console.log('reRendering molecule molFile');
-     ChemDoodle.readMOL(molecule);
-});
-
-*/
-
-$('document').ready(function(){
+     chemwriter.components['editor'].setMolfile(molFile);
 
 });
