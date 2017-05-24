477f134f1ed590832aa88d73748bf0dabd8c7ed8
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-05-24 12:38:32 -0700

    started server, created users router, experiment routers. Still needs to be tested

---
+++ labRouter.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {experiment} = require('./models');
+
+// when the root of this router is called with GET, return
+// all current experiment collection
+// endpoint diff but gets routed to /experiments by server.js
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/experiments.html');
+});
+
+router.get('/new', (req, res) => {
+  let user = req.user;
+  let userid = user._id
+  //TODO make new-experiment.html file and associated assets(.js & .css)
+  res.sendFile(__dirname + '/public/new-experiment.html');
+});
+
+router.get('/:id', (req, res) => {
+  //TODO make new-experiment.html file and associated assets(.js & .css)
+  res.sendFile(__dirname + '/public/experiment-edit.html');
+});
+
+//POST reqeust with specified requirements
+// when a new experiment is posted, make sure it has required content.
+// if not,log an error and return a 400 status code. if okay,
+// add new item to experiment and return it with a 201.
+router.post('/new', (req, res) => {
+  const requiredFields = ['category', 'title', 'author','purpose','procedure','results','conclusion'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  experment
+    .create({
+        id: this._id,
+        category: this.category,
+        title: this.title,
+        author: this.authorName,
+        purpose: this.title,
+        procedure: this.procedure,
+        results: this.results,
+        conclusion: this.conclusion,
+        created: this.created
+    })
+    .then(experimentEntry => res.status(201).json(experimentEntr.apiRepr()))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'Something went wrong'});
+    });
+
+});
+
+router.delete('/new/:id', (req, res) => {
+  experment
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      res.status(204).json({message: 'success'});
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'something went terribly wrong'});
+    });
+});
+
+router.put('/new/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['category', 'title', 'author','purpose','procedure','results','conclusion'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  experiment
+    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+    .exec()
+    .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+    .catch(err => res.status(500).json({message: 'Something went wrong'}));
+});
+
+
+router.delete('/:id', (req, res) => {
+  experiment
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      console.log(`Deleted blog post with id \`${req.params.ID}\``);
+      res.status(204).end();
+    });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const {DATABASE_URL, PORT} = require('./config');
+const {experiment} = require('./models');
+const labRouter = require('./labRouter');
+const {router: usersRouter} = require('./users');
+
+
+const app = express();
+
+
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//routers
+app.use('/users/', usersRouter);
+
+app.use('/experiments', labRouter);
+
+
+
+mongoose.Promise = global.Promise;
+
+//landing page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+//dashboard after log-in
+app.get('/dashboard',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.sendFile(__dirname + '/public/dashboard.html');
+  }
+);
+
+//shows collection of experiments
+app.get('/experiments',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.sendFile(__dirname + '/public/experiments.html');
+  }
+);
+
+//creates new experiments
+app.get('/experiments/new',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.sendFile(__dirname + '/public/new-experiment.html');
+  }
+);
+
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(PORT, () => {
+        console.log(`Your app is listening on port ${PORT}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
