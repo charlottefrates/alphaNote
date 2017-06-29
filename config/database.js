30252395003dd272999f5c2eacb011abe65f6db2
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-06-29 14:17:26 -0700

    changes database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost/experiments' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://admin:alpha@ds139942.mlab.com:39942/alphanote'//'mongodb://localhost/experiments' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
