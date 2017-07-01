51b1358f959f0d1248513749b2544bb6087cd8d9
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-06-30 23:06:41 -0700

    changed font t on edit

--- app/routes.js
+++ app/routes.js
@@ -22,19 +22,21 @@ module.exports = function(app, passport) {
      // show the login form
      app.get('/login', function(req, res) {
 
-          res.sendFile(path.join(__dirname, '../views', 'login.html'));
+         res.sendFile(path.join(__dirname, '../views', 'login.html'));
+         //res.render('login', { message: req.flash('loginMessage') });
+
      });
 
-     /*
-     app.get('/login', function(req, res) { // render the page and pass in any flash data if it exists res.render('login.ejs', { message: req.flash('loginMessage') }); });
-     */
 
 
      // process the login form
      app.post('/login', passport.authenticate('local-login', {
           successRedirect: '/dashboard', // redirect to the secure profile section
           failureRedirect: '/login', // redirect back to the signup page if there is an error
+          //failureMessage: "Invalid username or password",
           failureFlash: true // allow flash messages
+
+
      }));
 
      // =====================================
--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
+
 $('#home').on('click',function(event){
      event.preventDefault();
      window.location.href = `/`;
